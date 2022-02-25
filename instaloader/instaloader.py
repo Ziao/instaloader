f9b75d08cbb63328e02215bf5428a42ec4ac924e
Nick Kamer <nick@ziaomedia.com>
2022-02-25 15:31:24 +0100

    emoji

--- instaloader/instaloader.py
+++ instaloader/instaloader.py
@@ -149,6 +149,7 @@ class _PostPathFormatter(_ArbitraryItemFormatter):
         ret = ret.replace('/', '\u2215')
         ret = ret.replace('🫐', '{blueberry}')
         ret = ret.replace('🪴', '{potted-plant}')
+        ret = ret.replace('🥸', '{disguised-face}')
         if platform.system() == 'Windows':
             ret = ret.replace(':', '\uff1a').replace('<', '\ufe64').replace('>', '\ufe65').replace('\"', '\uff02')
             ret = ret.replace('\\', '\ufe68').replace('|', '\uff5c').replace('?', '\ufe16').replace('*', '\uff0a')
